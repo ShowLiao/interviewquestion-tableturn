b8476885d5b9cd7b354e0f4f496e3fcae232b1d6
ShowLiao <u8n.show@gmail.com>
2017-07-01 12:45:04 -0700

    Update timecls.java

--- src/timecls.java
+++ src/timecls.java
@@ -9,41 +9,41 @@ public class timecls {
 	}
 	
 	public static long getLCurrentTime() {
-		return mTime.getTime();
+	    return mTime.getTime();
 	}
 	
 	public static boolean diff10Mins(long time) {
 		
-		long tenMins = (mTime.getTime() - time);
+	    long tenMins = (mTime.getTime() - time);
 		
-		if ((tenMins-60*100) > 1)
-			return true;
-		else
-			return false;
+	    if ((tenMins-60*100) > 1)
+		return true;
+	    else
+		return false;
 		
 	}
 	
 	public static String getStrCurrentTime() {
-		return mTime.toString();
+	    return mTime.toString();
 	}
 	
 	public static String getDateLong2Str(long time) {
 		
-		Date date=new Date(time);
-        SimpleDateFormat df2 = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
-        String dateText = df2.format(date);
-        System.out.println(dateText);
-        return dateText;
+	    Date date=new Date(time);
+            SimpleDateFormat df2 = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+	    String dateText = df2.format(date);
+            System.out.println(dateText);
+            return dateText;
         
 	}
 	
 	public static String getTimeLong2Str(long time) {
 		
-		Date date=new Date(time);
-        SimpleDateFormat df2 = new SimpleDateFormat("HH:mm:ss");
-        String dateText = df2.format(date);
-        System.out.println(dateText);
-        return dateText;
+	    Date date=new Date(time);
+            SimpleDateFormat df2 = new SimpleDateFormat("HH:mm:ss");
+            String dateText = df2.format(date);
+            System.out.println(dateText);
+            return dateText;
         
 	}
 }
